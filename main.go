86f3f20c61f90b1a8a66b3c4e663968109a6dab4
Sathya <neelasathya06@gmail.com>
2025-07-31 23:02:08 -0400

    calling method

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	//var card = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	//card := "Ace of Spades"
+	//card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
