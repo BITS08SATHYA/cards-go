48fc8ebbaf9ea25dee9f3be5311eac3d7b67674a
Sathya <neelasathya06@gmail.com>
2025-07-31 23:52:53 -0400

    function calling

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
 	//var card = "Ace of Spades"
 	//card := "Ace of Spades"
 	//card = "Five of Diamonds"
-	card := newCard()
+	//card := newCard()
+	//
+	//fmt.Println(card)
 
-	fmt.Println(card)
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+
 }
 
 func newCard() string {
